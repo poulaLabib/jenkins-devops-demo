7cd9bb177ed2ae4102c795ea8d6540cca792abdd
Poula Labib <169151071+poulaLabib@users.noreply.github.com>
2026-08-19 16:34:16 +0300

    Add API integration test with MockMvc

--- src/test/java/com/poula/jenkinsdemo/JenkinsDemoApplicationTests.java
+++ src/test/java/com/poula/jenkinsdemo/JenkinsDemoApplicationTests.java
@@ -1,13 +1,26 @@
 package com.poula.jenkinsdemo;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class JenkinsDemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private MockMvc mockMvc;
 
+    @Test
+    void helloEndpointShouldReturnCorrectMessage() throws Exception {
+        mockMvc.perform(get("/api/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello from Jenkins!"));
+    }
 }
